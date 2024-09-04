aa382352823ccf718c0a2ad7bca7f202a71d02be
ValeriiKhodorishchenko <Khodorishchenko.Valerii@yandex.ru>
2024-09-04 16:47:37 +0500

    Связанные модели, вложенные сериализаторы

--- cats/serializers.py
+++ cats/serializers.py
@@ -1,13 +1,50 @@
 from rest_framework import serializers
 
-from .models import Cat, Owner
+from .models import Achievement, AchievementCat, Cat, Owner
+
+
+class AchievementSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Achievement
+        fields = ('id', 'name')
+
 
 class CatSerializer(serializers.ModelSerializer):
-    owner = serializers.StringRelatedField(read_only=True)
+    # owner = serializers.StringRelatedField(read_only=True)
+    achievements = AchievementSerializer(
+        required=False,  # Для добавления в БД без этого поля
+        # read_only=True,
+        many=True
+    )
 
     class Meta:
         model = Cat
-        fields = ('id', 'name', 'color', 'birth_year', 'owner')
+        fields = ('id', 'name', 'color', 'birth_year', 'owner', 'achievements')
+
+    def create(self, validated_data):
+        # Если в исходном запросе не было поля achievements
+        if 'achievements' not in self.initial_data:
+            # То создайм запись о котике без его достижений
+            cat = Cat.objects.create(**validated_data)
+            return cat
+        # Иначе делаем следующее:
+        # Уберём список достижений из словаря validated_data и сохраним его
+        achievements = validated_data.pop('achievements')
+        # Создаём нового котика пока без достижений, данных нам достаточно
+        cat =Cat.objects.create(**validated_data)
+        # Для каждого достижения из списка достижений
+        for achievement in achievements:
+            # Создадим новую запись или получим существующий экземпляр из БД
+            current_achievement, status = Achievement.objects.get_or_create(
+                **achievement
+            )
+            # Поместим ссылку на кождое достижение во вспомогательную таблицу
+            # Не забыв указать к какому котику оно относится
+            AchievementCat.objects.create(
+                achievement=current_achievement, cat=cat
+            )
+        return cat
 
 
 class OwnerSerializer(serializers.ModelSerializer):
