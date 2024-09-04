5a9f3210c4183c0485ca0760baa5677e0fbe4925
ValeriiKhodorishchenko <Khodorishchenko.Valerii@yandex.ru>
2024-09-04 17:00:59 +0500

    SerializerMethodField Дополнительнык поля, которых нет в БД

--- cats/serializers.py
+++ cats/serializers.py
@@ -1,3 +1,5 @@
+import datetime as dt
+
 from rest_framework import serializers
 
 from .models import Achievement, AchievementCat, Cat, Owner
@@ -17,10 +19,15 @@ class CatSerializer(serializers.ModelSerializer):
         # read_only=True,
         many=True
     )
+    age = serializers.SerializerMethodField()
 
     class Meta:
         model = Cat
-        fields = ('id', 'name', 'color', 'birth_year', 'owner', 'achievements')
+        fields = ('id', 'name', 'color', 'birth_year', 'owner', 'achievements',
+                  'age')
+        
+    def get_age(self, obj):
+        return dt.datetime.now().year - obj.birth_year
 
     def create(self, validated_data):
         # Если в исходном запросе не было поля achievements
